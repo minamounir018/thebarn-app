93201b26e38a4c5ea14f70412749cc4698a8c124
minamounir018 <71236247+minamounir018@users.noreply.github.com>
2023-03-13 21:09:10 +0200

    first commit

--- App.js
+++ App.js
@@ -1,20 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar, View, Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+import { createStackNavigator } from '@react-navigation/stack';
+import HomeScreen from './pages/HomeScreen';
+import ScheduleScreen from './pages/ScheduleScreen';
+import ProfileScreen from './pages/ProfileScreen';
+import SettingsScreen from './pages/SettingsScreen';
+import ScanScreen from './pages/ScanScreen';
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+export function TabNavigator({ navigation }) {
+  
+  return (
+    <Tab.Navigator screenOptions={({ route }) => ({
+      tabBarIcon: ({ focused, color, size }) => {
+        let iconName;
+        if (route.name === 'Home') {
+          iconName = 'home';
+        } else if (route.name === 'Profile') {
+          iconName = 'user-alt';
+        } else if (route.name === 'Scan') {
+          iconName = 'camera';
+        } else if (route.name === 'Schedule') {
+          iconName = 'calendar-alt';
+        } else if (route.name === 'Settings') {
+          iconName = 'sliders-h';
+        }
+
+        return (
+          <View style={route.name === 'Scan' ? { backgroundColor: 'white', borderColor: '#7C0A02', borderRadius: '50%', borderWidth: 2, position: 'absolute', bottom: 20 } : {}}>
+            {route.name === 'Scan' ? (
+              <Image
+                resizeMode={'cover'}
+                style={{ width: 50, height: 50, borderRadius: 50 }}
+                source={require("./assets/logo.png")}
+              />
+            ) : (
+              <Icon name={iconName} size={size} color={color} />
+            )}
+          </View>
+        )
+      },
+      tabBarActiveTintColor: '#7C0A02',
+      tabBarInactiveTintColor: 'gray',
+      tabBarShowLabel: false
+    })}>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+      <Tab.Screen
+        listeners={() => ({
+          tabPress: (e) => {
+            e.preventDefault();
+            navigation.navigate("Scan");
+          },
+        })}
+        name="Scan" component={ProfileScreen} />
+      <Tab.Screen name="Schedule" component={ScheduleScreen} />
+      <Tab.Screen name="Settings" component={SettingsScreen} />
+    </Tab.Navigator>
+  )
+}
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StatusBar
+        barStyle={"dark-content"}
+      />
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
+        <Stack.Screen name="main" component={TabNavigator} />
+        <Stack.Screen
+          options={{
+            presentation: 'modal'
+          }}
+          name="Scan" component={ScanScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
---
+++ pages/ScanScreen/index.jsx
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+
+export default function ScanScreen({ navigation }) {
+  const [hasPermission, setHasPermission] = useState(null);
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+
+    const getBarCodeScannerPermissions = async () => {
+      const { status } = await BarCodeScanner.requestPermissionsAsync();
+      setHasPermission(status === 'granted');
+    };
+
+    getBarCodeScannerPermissions();
+  }, []);
+
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    // navigation.navigate("main");
+    alert(`Bar code has been scanned!`);
+  };
+
+  if (hasPermission === null) {
+    return <Text>Requesting for camera permission</Text>;
+  }
+  if (hasPermission === false) {
+    return <Text>No access to camera</Text>;
+  }
+
+
+  return (
+    <View style={styles.container}>
+      <BarCodeScanner
+        onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+        style={StyleSheet.absoluteFillObject}
+      />
+      {scanned ? <Button title={'Tap to Scan Again'} onPress={() => setScanned(false)} />
+        : <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ borderRadius: 20, borderColor: 'gray', borderWidth: 3, width: 300, height: 300 }} />
+        </View>}
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'column',
+    justifyContent: 'center',
+  },
+});
